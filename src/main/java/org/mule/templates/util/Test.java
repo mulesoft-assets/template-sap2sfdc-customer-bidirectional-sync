1cbfa6007cfff2ce94d5e365978f3c38d58d4d4f
Marek <marecica2@gmail.com>
2014-07-29 10:28:38 +0200

    initial commit

---
+++ src/main/java/org/mule/templates/util/Test.java
@@ -0,0 +1,16 @@
+package org.mule.templates.util;
+
+import org.joda.time.DateTime;
+
+
+public class Test {
+	public static void main(String[] args){
+		System.err.println("ssss");
+		DateTime d = org.mule.templates.date.DateUtils.ISOStringDateToDateTime("2014-07-22T17:38:15Z");
+		DateTime d1 = org.mule.templates.date.DateUtils.ISOStringDateToDateTime("2014-07-23T17:38:15Z");
+		DateTime d2 = org.mule.templates.date.DateUtils.stringToDateTimeWithProvidedOffset("2014-07-21T17:38:15Z", "yyyy-MM-dd'T'HH:mm:ss'Z'", "-00:10:20");
+		System.err.println(d2);
+		System.err.println(d.compareTo(d2));
+		boolean t = org.mule.templates.date.DateUtils.stringToDateTimeWithProvidedOffset("2014-07-21T17:38:15.000Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "-00:01:05").compareTo(org.mule.templates.date.DateUtils.stringToDateTimeWithProvidedOffset("2014-07-21T17:38:15.000Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "+00:00:00")) > 0;
+	}
+}
---
+++ src/test/java/org/mule/templates/integration/BusinessLogicIT.java
@@ -0,0 +1,108 @@
+/**
+ * Mule Anypoint Template
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ */
+
+package org.mule.templates.integration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mule.MessageExchangePattern;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
+
+import com.mulesoft.module.batch.BatchTestHelper;
+
+/**
+ * The objective of this class is to validate the correct behavior of the flows
+ * for this Anypoint Tempalte that make calls to external systems.
+ * 
+ */
+public class BusinessLogicIT extends AbstractTemplateTestCase {
+	protected static final String TEMPLATE_NAME = "customer-migration";
+	protected static final int TIMEOUT_SEC = 120;
+	private BatchTestHelper helper;
+	
+	protected SubflowInterceptingChainLifecycleWrapper createAccountSapFlow;
+	protected SubflowInterceptingChainLifecycleWrapper deleteAccountFromSapFlow;
+	protected SubflowInterceptingChainLifecycleWrapper retrieveAccountFromSalesforceFlow;
+	protected SubflowInterceptingChainLifecycleWrapper deleteAccountFromSalesforceFlow;
+	
+	private String existingCustomerNameInSap = "Nelson Tax & Associates";
+	Map<String, Object> sfAccount;
+	
+	@Before
+	public void setUp() throws Exception {
+		helper = new BatchTestHelper(muleContext);
+		
+		createAccountSapFlow = getSubFlow("createAccountSapFlow");
+		createAccountSapFlow.initialise();
+		
+		deleteAccountFromSapFlow = getSubFlow("deleteAccountFromSapFlow");
+		deleteAccountFromSapFlow.initialise();
+		
+		retrieveAccountFromSalesforceFlow = getSubFlow("retrieveAccountFromSalesforceFlow");
+		retrieveAccountFromSalesforceFlow.initialise();
+		
+		deleteAccountFromSalesforceFlow = getSubFlow("deleteAccountFromSalesforceFlow");
+		deleteAccountFromSalesforceFlow.initialise();
+		
+		createSapTestData();
+	}
+
+	@After
+	public void tearDown() throws MuleException, Exception {
+		deleteSapTestData();
+		deleteSalesforceTestData();
+	}
+
+	@Test
+	public void testMainFlow() throws Exception {
+		runFlow("triggerFlow");
+		
+		// Wait for the batch job executed by the poll flow to finish
+		helper.awaitJobTermination(TIMEOUT_SEC * 1000, 500);
+		helper.assertJobWasSuccessful();
+		
+		Map<String, Object> payload = new HashMap<String, Object>();
+		payload.put("Name", existingCustomerNameInSap);
+		MuleEvent event = retrieveAccountFromSalesforceFlow.process(getTestEvent(payload, MessageExchangePattern.REQUEST_RESPONSE));
+		sfAccount = (Map<String, Object>) event.getMessage().getPayload();
+		Assert.assertNotNull(sfAccount);
+		
+		
+	}
+	
+	private void createSapTestData() throws MuleException, Exception{
+		String uniqueSuffix = "_" + TEMPLATE_NAME + "_" + System.currentTimeMillis();
+		
+		Map<String, Object> sapAccount3 = new HashMap<String, Object>();
+		sapAccount3.put("Name", "Name_SAP" + uniqueSuffix);
+		List<Map<String, Object>> createdAccountInSap = new ArrayList<Map<String, Object>>();
+		createdAccountInSap.add(sapAccount3);
+	
+		MuleEvent event = createAccountSapFlow.process(getTestEvent(createdAccountInSap, MessageExchangePattern.REQUEST_RESPONSE));
+		System.err.println("SAP RESPONSE");
+		System.err.println(event.getMessage().getPayload());
+	}
+
+	private void deleteSapTestData(){
+		
+	}
+
+	private void deleteSalesforceTestData() throws MuleException, Exception{
+		List<String> idList = new ArrayList<String>();
+		idList.add((String)sfAccount.get("Id"));
+		MuleEvent event = deleteAccountFromSalesforceFlow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+	}
+
+}
